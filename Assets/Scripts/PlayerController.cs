ce8cd82c517b1bf889d87c2595a00d9e5ee70852
Mavinyx <nicollyvianamartins@gmail.com>
2026-06-13 17:49:29 -0300

    Primeiro commit - Insercao do Tilemap, Assets, Movimentacao Basica

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D corpo;
+    private SpriteRenderer sprite;
+    private Animator anim;
+    [SerializeField] private float vel = 3.0f;
+
+    void Start()
+    {
+        // Initialize player settings here
+        corpo = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        // Handle player input and movement here
+
+        //pega tanto as setinhas quanro a e d
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        corpo.linearVelocity = new Vector2(moveHorizontal * vel, moveVertical * vel);
+        anim.SetFloat("speed", Mathf.Abs(moveHorizontal)); // Update animation speed based on horizontal movement
+        Flip(moveHorizontal);
+    }
+
+    //funçao para virar o personagem para a direita ou esquerda dependendo da tecla pressionada
+    void Flip(float moveHorizontal)
+    {
+        if (moveHorizontal > 0)
+        {
+            sprite.flipX = false; // Facing right
+        }
+        else if (moveHorizontal < 0)
+        {
+            sprite.flipX = true; // Facing left
+        }
+    }
+}
