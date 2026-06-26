4ceef13a8a027b086c2dcd0d5ea7de6f294d0868
Mavinyx <nicollyvianamartins@gmail.com>
2026-06-25 21:58:38 -0300

    feat: mecanica de ataque | fix: arrumei a estrutura das funções

--- Assets/Scripts/MushController.cs
+++ Assets/Scripts/MushController.cs
@@ -4,7 +4,8 @@ public class MushController : MonoBehaviour
 {
     [SerializeField] private float vel;
     private Transform alvo;
-    
+    public int vida = 1;
+
     void Start()
     {
         alvo = GameObject.FindGameObjectWithTag("Player").transform;
@@ -14,8 +15,16 @@ public class MushController : MonoBehaviour
     {
         if (alvo != null)
         {
-        
             transform.position = Vector2.MoveTowards(transform.position, alvo.position, vel * Time.deltaTime);
+        }
+    }
+
+    public void LevarDano(int dano)
+    {
+        vida -= dano;
+        if (vida <= 0)
+        {
+            Destroy(gameObject);
         }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,22 @@
 using UnityEngine;
 
+// Ver se o ataque ta com hitbox pros dois lado
+// transformar esses valores avulsos em constante
+// incrementar a macaninca pra qnd o inimigo estiver com vida em 1, pausa e fazer UI
 public class PlayerController : MonoBehaviour
 {
+
+    // Variáveis privadas para armazenar referências aos componentes do jogador
     private Rigidbody2D corpo;
     private SpriteRenderer sprite;
     private Animator anim;
     [SerializeField] private float vel = 3.0f;
+
+    //Variaveis para controlar o ataque do jogador
+    [SerializeField] private Transform attackArea;
+    private float alcanceAtque= 0.5f;
+    private int danoAtaque = 1;
+    [SerializeField] private LayerMask camadaInimigo;
 
     void Start()
     {
@@ -16,10 +27,40 @@ public class PlayerController : MonoBehaviour
     }
     void Update()
     {
-        // Handle player input and movement here
-
+      
+    //--------------------MOVIMENTO DO JOGADOR--------------------
         //pega tanto as setinhas quanro a e d
-        float moveHorizontal = Input.GetAxis("Horizontal");
+        MovimentoPlayer();
+
+    //--------------------ATAQUE DO JOGADOR--------------------
+        AtaquePlayer();
+
+    }
+
+    //função para controlar o ataque do jogador
+    void AtaquePlayer()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl) || Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger("Attack");
+            //Physics2D.OverlapCircleAll faz com que a unity desenhe um circulo na tela e detecte todos os inimigos dentro desse circulo, nesse caso o circulo é desenhado na posição do objeto attackArea, com o raio de alcanceAtque e apenas na camada camadaInimigo
+            Collider2D[] inimigos = Physics2D.OverlapCircleAll(attackArea.position, alcanceAtque, camadaInimigo);
+            foreach (Collider2D inimigo in inimigos)
+            {
+                var inimigoScript = inimigo.GetComponent<MushController>();
+                if (inimigoScript != null)
+                {
+                    inimigoScript.LevarDano(danoAtaque);
+                }
+            }
+        }
+   
+    }
+
+    //função para controlar o movimento do jogador
+    void MovimentoPlayer()
+    {
+           float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         corpo.linearVelocity = new Vector2(moveHorizontal * vel, moveVertical * vel);
         anim.SetFloat("speed", Mathf.Abs(moveHorizontal)); // Update animation speed based on horizontal movement
@@ -32,7 +73,6 @@ public class PlayerController : MonoBehaviour
         anim.SetFloat("MoveX", movimento.x);
         anim.SetFloat("MoveY", movimento.y);
         anim.SetFloat("speed", movimento.sqrMagnitude);
-
     }
 
     //funçao para virar o personagem para a direita ou esquerda dependendo da tecla pressionada
